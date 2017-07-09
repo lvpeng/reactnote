f61fd30330de64ba1ae7b34855d3cbc1eb7cfd86
EvanLyu <lvpeng-333@163.com>
2017-07-09 10:28:54 +0800

    Adding Local State to a Class

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-//Converting a Function to a Class
+//Adding Local State to a Class
 class Clock extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = { date: new Date() }
+    }
     render(){
         return (
             <div>
-                <h1>Hello, world</h1>
-                <h2>{this.props.date.toLocaleTimeString()}</h2>
+                <h1>Hello, Clock</h1>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
             </div>
         )
     }
 }
 
-/*
-Clock is now defined as a class rather than a function.
-
-This lets us use additional features such as local state and lifecycle hooks.
-*/
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+)
