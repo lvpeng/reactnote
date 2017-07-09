a502f229dcdfd4000bf5ccc7bdea8ae06d06f193
EvanLyu <lvpeng-333@163.com>
2017-07-10 05:41:22 +0800

    Handling Events -1

--- App.js
+++ App.js
@@ -2,66 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 /*
-The Data Flows Down
+    Handling Events
 */
-// FormattedDate Child Component
-function FormattedDate(props){
-    return (
-        <h2>{props.date.toLocaleTimeString()}</h2>
-    )
-}
 
-class Clock extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            date: new Date()
-        }
+// Button Component
+function Button(){
+
+    function handleClick(){
+      console.log('activateLasers');
     }
 
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick()
-            ,1000)    
-    }
-
-    componentWillUnmount () {
-        clearInterval(this.timerID)
-    }
-    
-    tick(){
-        this.setState({
-            date: new Date()
-        })
-    }
-
-    // This is why state is often called local or encapsulated. It is not accessible to any component other than the one that owns and sets it.
-    render(){
-        return (
-            <div>
-                <h1>Clock</h1>,
-                <FormattedDate date={this.state.date}/>
-            </div>
-        )
-    }
-} 
-
-// Each Clock sets up its own timer and updates independently.
-function App(){
     return (
         <div>
-            <Clock />
-            <Clock />
-            <Clock />
+            <button onClick={handleClick}>
+                Activate Lasers
+            </button>
         </div>
     )
 }
 
+// ActionLink Component 
+function ActionLink(){
+
+    function handleClick(e){
+        // Another difference is that you cannot return false to prevent default behavior in React. You must call preventDefault explicitly.
+        e.preventDefault();
+        console.log('The link was clicked')
+    }
+
+    return (
+        <a href="#" onClick={handleClick}>Click Me</a>
+    )
+}
+
+
+
 ReactDOM.render(
-    <App />,
-    document.getElementById("root")
+    <div>
+        <Button />
+        <ActionLink />
+    </div>,
+    document.getElementById('root')
 )
 
-/*This is commonly called a "top-down" or "unidirectional" data flow. Any state is always owned by some specific component, and any data or UI derived from that state can only affect components "below" them in the tree.*/
+/*Handling events with React elements is very similar to handling events on DOM elements. There are some syntactic differences:
 
-/*In React apps, whether a component is stateful or stateless is considered an implementation detail of the component that may change over time. You can use stateless components inside stateful components, and vice versa.*/
+- React events are named using `camelCase`, rather than lowercase.
+- With JSX you pass a function as the event handler, rather than a string.*/
