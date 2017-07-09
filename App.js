b334f7272bf61d9c8c046c4460f2d77af84a03e2
EvanLyu <lvpeng-333@163.com>
2017-07-09 09:51:13 +0800

    Clock Component : update itself every second

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function sum(a, b){
-    return a + b
+function Clock(props){
+    return (
+        <div>
+            <h1>Hello, world</h1>
+            <h2>{ props.date.toLocaleTimeString() }</h2>
+        </div>
+    )
 }
 
-function withdraw(account, amount) {
-    account.total -= amount
+function tick(){
+    ReactDOM.render(
+        <Clock date={new Date()} />,
+        document.getElementById('root')    
+    )
 }
 
-/*
-    All React components must act like `pure` function with respect to their props
-*/
-
-
-/*
-    Of course, application UIs are dynamic and change over time. In the next section, we will introduce a new concept of "state". State allows React components to change their output over time in response to user actions, network responses, and anything else, without violating this rule.
-*/
+setInterval(tick, 1000)
