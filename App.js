3ae261432056457f8e73caf78c31e2fa0d3063b6
EvanLyu <lvpeng-333@163.com>
2017-07-09 10:18:23 +0800

    before add `State`

--- App.js
+++ App.js
@@ -10,11 +10,22 @@ function Clock(props){
     )
 }
 
-function tick(){
-    ReactDOM.render(
-        <Clock date={new Date()} />,
-        document.getElementById('root')    
-    )
-}
+/*
+However, it misses a crucial requirement: the fact that the Clock sets up a timer and updates the UI every second "should be an implementation detail of the Clock.""
 
-setInterval(tick, 1000)
+Ideally we want to write this once and have the Clock update itself:    
+
+To implement this, we need to add "state" to the Clock component.
+
+State is similar to props, but it is private and fully controlled by the component.
+
+We mentioned before that components defined as classes have some additional features. Local state is exactly that: a feature available only to classes.
+
+
+*/
+
+
+ReactDOM.render(
+    <Clock date={new Date()}/>,
+    document.getElementById('root')    
+)
