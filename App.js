c4012f163b0b62455fd74c8a48a4351ea39fff79
EvanLyu <lvpeng-333@163.com>
2017-07-09 17:37:16 +0800

    The Data Flow Down

--- App.js
+++ App.js
@@ -1,60 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
 /*
-    State Updates are Merged
+The Data Flows Down
 */
-class Article extends React.Component{
+// FormattedDate Child Component
+function FormattedDate(props){
+    return (
+        <h2>{props.date.toLocaleTimeString()}</h2>
+    )
+}
+
+class Clock extends React.Component{
     constructor(props){
         super(props)
-        this.state = { 
-            posts : [],
-            comments: []
+        this.state = {
+            date: new Date()
         }
     }
 
-/* When you call setState(), React merges the object you         provide        into the current state.
-
-    The merging is shallow, so this.setState({comments}) leaves this.state.posts intact, but completely replaces this.state.comments.
-
-
-*/    
     componentDidMount() {
-        fetchPosts().then( result  => {
-            this.setState({
-                posts: result.data.posts
-            })
+        this.timerID = setInterval(
+            () => this.tick()
+            ,1000)    
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.timerID)
+    }
+    
+    tick(){
+        this.setState({
+            date: new Date()
         })
-        fetchComments().then( result => {
-            this.setState({
-                comments: result.data.comments
-            })
-        })
     }
 
-
-    fetchPosts(){
-        //...
-        
-    }
-    fetchComments(){
-        //..
-    }
-
+    // This is why state is often called local or encapsulated. It is not accessible to any component other than the one that owns and sets it.
     render(){
         return (
             <div>
-                <h1>Article List</h1>
-                <ul>
-                    {}
-                </ul>
-            </div>    
+                <h1>Clock</h1>,
+                <FormattedDate date={this.state.date}/>
+            </div>
         )
     }
+} 
+
+// Each Clock sets up its own timer and updates independently.
+function App(){
+    return (
+        <div>
+            <Clock />
+            <Clock />
+            <Clock />
+        </div>
+    )
 }
 
 ReactDOM.render(
-    <Article />,
+    <App />,
     document.getElementById("root")
-)     
+)
+
+/*This is commonly called a "top-down" or "unidirectional" data flow. Any state is always owned by some specific component, and any data or UI derived from that state can only affect components "below" them in the tree.*/
+
+/*In React apps, whether a component is stateful or stateless is considered an implementation detail of the component that may change over time. You can use stateless components inside stateful components, and vice versa.*/
