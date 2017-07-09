6cf3f3676afae44d5320fdad9cbd33cec39d85ce
EvanLyu <lvpeng-333@163.com>
2017-07-09 10:22:08 +0800

    Converting a Function to a Class

--- App.js
+++ App.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Clock(props){
-    return (
-        <div>
-            <h1>Hello, world</h1>
-            <h2>{ props.date.toLocaleTimeString() }</h2>
-        </div>
-    )
+//Converting a Function to a Class
+class Clock extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello, world</h1>
+                <h2>{this.props.date.toLocaleTimeString()}</h2>
+            </div>
+        )
+    }
 }
 
 /*
-However, it misses a crucial requirement: the fact that the Clock sets up a timer and updates the UI every second "should be an implementation detail of the Clock.""
+Clock is now defined as a class rather than a function.
 
-Ideally we want to write this once and have the Clock update itself:    
-
-To implement this, we need to add "state" to the Clock component.
-
-State is similar to props, but it is private and fully controlled by the component.
-
-We mentioned before that components defined as classes have some additional features. Local state is exactly that: a feature available only to classes.
-
-
+This lets us use additional features such as local state and lifecycle hooks.
 */
-
-
-ReactDOM.render(
-    <Clock date={new Date()}/>,
-    document.getElementById('root')    
-)
