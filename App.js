d314c9e2ca4cf597d3b3dc1e22d651010f695713
EvanLyu <lvpeng-333@163.com>
2017-07-10 11:26:45 +0800

    example: renders a different greeting depending on the value of isLoggedIn prop.

--- App.js
+++ App.js
@@ -2,72 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 /*
-    Handling Events
+    Conditional Rendering
 */
-
-// Button Component
-function Button(){
-    function handleClick(){
-      console.log('activateLasers');
-    }
-
-    return (
-        <div>
-            <button onClick={handleClick}>
-                Activate Lasers
-            </button>
-        </div>
-    )
+function UserGreeting(){
+    return <h1>Welcome back</h1>
+}
+function GuestGreeting(){
+    return <h1>Sign up</h1>
 }
 
-// ActionLink Component 
-function ActionLink(){
-
-    function handleClick(e){
-        // Another difference is that you cannot return false to prevent default behavior in React. You must call preventDefault explicitly.
-        e.preventDefault();
-        console.log('The link was clicked')
+function Greeting(props){
+    const isLoggedIn = props.isLoggedIn
+    if(isLoggedIn){
+        return <UserGreeting />
     }
-
-    return (
-        <a href="#" onClick={handleClick}>Click Me</a>
-    )
+    return <GuestGreeting / >
 }
 
-class Toggle extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            isToggleOn: false
-        }
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this)
-    }
-
-    handleClick(){
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
-        }))
-    }
-
-    render(){
-        return (
-            // `this.handleClick` , not `handleClick`
-            <button onClick={this.handleClick}>{this.state.isToggleOn ? "ON" : "OFF"}</button>
-        )
-    }
-}
 
 ReactDOM.render(
-    <div>
-        <Button />
-        <ActionLink />
-        <Toggle />
-    </div>,
+    <Greeting isLoggedIn={true} />,
     document.getElementById('root')
 )
-/*
-    This is not React-specific behavior; it is a part of how functions work in JavaScript. Generally, if you refer to a method without () after it, such as onClick={this.handleClick}, you should bind that method.
-*/
 
-// This binding is necessary to make `this` work in the callback
+// This example renders a different greeting depending on the value of isLoggedIn prop.
